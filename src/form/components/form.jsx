7eb8fb215ec0bf2afe5bf9b043293088090e176c
polin103109 <sabrinpolinece18@gmail.com>
2023-04-23 21:06:23 +0600

    updated form-field

--- src/form/components/form.jsx
+++ src/form/components/form.jsx
@@ -1,14 +1,27 @@
 import './form.css';
+function Formgroup({
+   label,
+   fieldName,
+   type
+}){
+    return (
+        <div className='form-inputgrp'>
+        <label>{`${label}:`}</label>
+     <input name={fieldName} type={type}/>
+        </div>
+       );
+}
 
-function Form() {
-    return <div className="form-container">
+
+function Form(){
+    return (
+    <div className="form-container">
         <form class="product-form">
-            <div className='form-inputgrp'>
-            <label>Product ID: </label>
-         <input name='id' type={'number'}/>
-            </div>
-           
+          <Formgroup label="Product ID"  fieldName="id" type="number"/>
+          <Formgroup label="Product Name"  fieldName="name" type="text"/>
+          <Formgroup label="Product Price"  fieldName="number" type="number"/>
         </form>
-    </div>;
+    </div>
+    );
   }
 export default Form;
