76a3436d22e19269f674ff39d3a4056a475e8e17
polin103109 <sabrinpolinece18@gmail.com>
2023-04-25 17:28:41 +0600

    started authentication

--- src/form/components/productForm/form.jsx
+++ src/form/components/productForm/form.jsx
@@ -16,35 +16,46 @@ function Form(){
    
 function handleFormSubmit(event){
     event.preventDefault();
+  if (product.id.trim() === '' || product.name.trim() === '' || product.price.trim() === '') {
+    alert('Please fill out all fields');
+    return;
+  }
+
     const checkEmptyInput = !Object.values(product).every(res=>res==="")
     if(checkEmptyInput){
       const newData = (data) => (
         [...data,product])
-        setTableData(newData );
+        setTableData(newData(tableData));
+        // setTableData(newData );
         const emptyInput = {
           id:'',
           name:"",
           price:'',
         }
+        
         setProduct(emptyInput )
     }
     }
     
    
-function handleOnChange(e){
-  const newInput = (data) =>(
-    {...data,
-   [e.target.name]:e.target.value});
-   setProduct(newInput);
+// function handleOnChange(e){
+//   const newInput = (data) =>(
+//     {...data,
+//    [e.target.name]:e.target.value});
+//    setProduct(newInput);
+//     }
+    function handleOnChange(e){
+      const newInput = (data) => ({...data, [e.target.name]:e.target.value});
+      setProduct(newInput(product));
     }
 
     return (
     <div className="total-div">
       <div className='form-container'>
         <form class="product-form" onSubmit={handleFormSubmit}>
-          <Formgroup label="Product ID"  fieldName="id" type="number" onChange={handleOnChange} value={product.id} />
-          <Formgroup label="Product Name"  fieldName="name" type="text" onChange={handleOnChange} value={product.name}/>
-          <Formgroup label="Product Price"  fieldName="price" type="number" onChange={handleOnChange} value={product.price}/>
+          <Formgroup label="Product ID"   fieldName="id" type="number" onChange={handleOnChange} value={product.id} />
+          <Formgroup label="Product Name"   fieldName="name" type="text" onChange={handleOnChange} value={product.name}/>
+          <Formgroup label="Product Price"   fieldName="price" type="number" onChange={handleOnChange} value={product.price}/>
 
           <div className='product-form-footer'>
             <input type="reset" value="Reset"/>
